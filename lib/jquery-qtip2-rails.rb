f5e725ac35de12b50e4fb4e3835ff26137bd17e5
Tanguy Krotoff <tkrotoff@gmail.com>
2012-05-14 20:17:31 +0200

    Create jquery-qtip2-rails gem Command 'bundle gem jquery-qtip2-rails'

---
+++ lib/jquery-qtip2-rails.rb
@@ -0,0 +1,9 @@
+require "jquery-qtip2-rails/version"
+
+module Jquery
+  module Qtip2
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery-qtip2-rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Qtip2
+    module Rails
+      VERSION = "0.0.1"
+    end
+  end
+end
