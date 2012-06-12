5c6d422f2302f50ecdb1bdbaa15402a426d18988
Tanguy Krotoff <tkrotoff@gmail.com>
2012-06-12 11:51:21 +0200

    Add a dummy application in order to test jquery-qtip2-rails

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '86b09ada9b1514391d3b581063bfdbe17769bf8bbdf1de008f56dc3ef82310073d8bcd6bbc91e147a97296fa8853e88bad79664f20cf4d1c5c2d9309230748ae'
---
+++ test/dummy/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+
+  mount Jquery::Qtip2::Rails::Engine => "/jquery-qtip2-rails"
+end
